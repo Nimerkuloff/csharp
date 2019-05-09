37801e74a5ecfcf31b613cdeea5e15759234d045
nimerkuloff <komander-nick@yandex.ru>
2019-05-09 10:27:45 +0700

    draw function; sliders don't work

---
+++ lab7/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBarY = new System.Windows.Forms.TrackBar();
+            this.trackBarZ = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBarY
+            // 
+            this.trackBarY.LargeChange = 1;
+            this.trackBarY.Location = new System.Drawing.Point(632, 39);
+            this.trackBarY.Maximum = 100;
+            this.trackBarY.Minimum = 1;
+            this.trackBarY.Name = "trackBarY";
+            this.trackBarY.Size = new System.Drawing.Size(104, 21);
+            this.trackBarY.TabIndex = 0;
+            this.trackBarY.Value = 1;
+            this.trackBarY.ValueChanged += new System.EventHandler(this.TrackBarY_ValueChanged);
+            // 
+            // trackBarZ
+            // 
+            this.trackBarZ.LargeChange = 1;
+            this.trackBarZ.Location = new System.Drawing.Point(632, 110);
+            this.trackBarZ.Maximum = 100;
+            this.trackBarZ.Minimum = 1;
+            this.trackBarZ.Name = "trackBarZ";
+            this.trackBarZ.Size = new System.Drawing.Size(104, 21);
+            this.trackBarZ.TabIndex = 1;
+            this.trackBarZ.Value = 1;
+            this.trackBarZ.ValueChanged += new System.EventHandler(this.TrackBarZ_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(629, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ось У";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(632, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ось Z";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBarZ);
+            this.Controls.Add(this.trackBarY);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZ)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarY;
+        private System.Windows.Forms.TrackBar trackBarZ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab7/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+
+        private double xMin = 0;
+        private double xMax = 6 * Math.PI;
+        private double step = (Math.PI * 2) / 10;
+
+        private double[] x;
+        private double[] y;
+        private double[] z;
+        private double[] v;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Chart chart;
+        private void CalcFunction()
+        {
+
+            // Количество точек графика
+            int count = (int)Math.Ceiling((xMax - xMin) / step) + 1;
+            // Создаѐм массивы нужных размеров
+            x = new double[count];
+            y = new double[count];
+            z = new double[count];
+            v = new double[count];
+            // Расчитываем точки для графиков функции
+            for (int i = 0; i < count; i++)
+            {
+                x[i] = xMin + step * i;
+                y[i] = (trackBarY.Value / trackBarY.Maximum) * Math.PI;
+                z[i] = (trackBarZ.Value / trackBarZ.Maximum) * Math.PI;
+
+                v[i] = Math.Pow(Math.Abs(Math.Cos(x[i]) - Math.Cos(y[i])), 1 + 2 * Math.Pow(Math.Sin(y[i]), 2))
+                       * (1 + z[i] + Math.Pow(z[i], 2) / 2 + Math.Pow(z[i], 3) / 3 + Math.Pow(z[i], 4) / 4);
+
+            }
+
+        }
+        private void CreateChart()
+        {
+            chart = new Chart();
+            chart.Parent = this;
+            chart.SetBounds(10, 10, ClientSize.Width - 20, ClientSize.Height - 20);
+
+            ChartArea area = new ChartArea();
+            area.Name = "myGraph";
+            area.AxisX.Minimum = xMin;
+            area.AxisX.Maximum = xMax;
+            area.AxisX.MajorGrid.Interval = step;
+
+            chart.ChartAreas.Add(area);
+
+            Series series = new Series();
+            series.ChartArea = "myGraph";
+            series.ChartType = SeriesChartType.Spline;
+            series.BorderWidth = 3;
+            series.LegendText = "V";
+            chart.Series.Add(series);
+
+            Legend legend = new Legend();
+            chart.Legends.Add(legend);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateChart();
+            CalcFunction();
+            chart.Series[0].Points.DataBindXY(x, v);
+        }
+
+        private void TrackBarY_ValueChanged(object sender, EventArgs e)
+        {
+            chart.Series[0].Points.Clear();
+            CalcFunction();
+            chart.Series[0].Points.DataBindXY(x, v);
+        }
+
+        private void TrackBarZ_ValueChanged(object sender, EventArgs e)
+        {
+            chart.Series[0].Points.Clear();
+            CalcFunction();
+            chart.Series[0].Points.DataBindXY(x, v);
+        }
+    }
+}
